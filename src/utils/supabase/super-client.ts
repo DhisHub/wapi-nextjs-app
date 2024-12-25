4ea2474c142c5018da053c101d32450cb5a39640
Awsomeman22 <abdirahmanbashirosman@gmail.com>
2024-12-25 14:47:24 +0800

    added NEXT_PUBLIC on .env.local file

--- src/utils/supabase/super-client.ts
+++ src/utils/supabase/super-client.ts
@@ -3,5 +3,5 @@ import { createBrowserClient } from "@supabase/ssr";
 export const createCuClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!,
   );
