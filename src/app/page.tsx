50e1eb1f06dd0587694d9d951bcbd6c59d0cf1c7
Awsomeman22 <abdirahmanbashirosman@gmail.com>
2025-01-01 14:44:23 +0800

    customized the metadata of the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,52 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
-  // other metadata
+  title: "Wapi.so - Effortless WhatsApp API for Developers 🚀",
+  description:
+    "Discover Wapi.so, an open-source, simple, and reliable WhatsApp API. Easily send texts, files, voice, and videos, and create multiple sessions. Perfect for developers who need a seamless way to integrate WhatsApp.",
+  keywords: [
+    "WhatsApp API",
+    "WhatsApp integration",
+    "open-source WhatsApp API",
+    "WhatsApp developer tools",
+    "send text WhatsApp API",
+    "WhatsApp file API",
+    "WhatsApp voice API",
+    "WhatsApp video API",
+    "create WhatsApp sessions",
+    "developer-friendly WhatsApp API",
+    "Wapi.so",
+  ],
+  creator: "Wapi.so Team",
+  robots: "index, follow",
+  openGraph: {
+    title: "Wapi.so - Simplify WhatsApp API Integration",
+    description:
+      "Wapi.so is your go-to open-source solution for WhatsApp API integration, featuring text, file, voice, and video sending capabilities, and the ability to create multiple sessions.",
+    url: "https://wapi.so",
+    images: [
+      {
+        url: "https://wapi.so/images/screenshot/wapi.png", // Link to the image you want to show
+        width: 1200,
+        height: 630,
+        alt: "Wapi.so WhatsApp API",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image", // Ensures large image preview on Twitter
+    title: "Wapi.so - Effortless WhatsApp API for Developers",
+    description:
+      "Wapi.so offers developers a simple way to integrate WhatsApp with features like text, file, voice, and video sending. Easily manage multiple sessions with an open-source API.",
+    images: [
+      {
+        url: "https://wapi.so/images/screenshot/wapi.png", // Link to the image you want to show
+        alt: "Wapi.so WhatsApp API",
+      },
+    ],
+    creator: "@wapi_so",
+  },
 };
 
 export default function Home() {
